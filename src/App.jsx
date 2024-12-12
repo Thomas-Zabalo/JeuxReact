3757624094719cb259fb6a5f32564a30c2561bba
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-12-12 16:06:50 +0100

    jeux

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import * as THREE from 'three';
-import * as CANNON from 'cannon-es'
+import * as CANNON from 'cannon-es';
 import CannonDebugger from 'cannon-es-debugger';
 
 function App() {
 
   useEffect(() => {
 
-    const pointsUI = document.getElementById("points")
-    let points = 0
+    const pointsUI = document.getElementById("points");
+    let points = 0;
     let gameOver = false;
 
     // Scene setup
@@ -26,123 +26,83 @@ function App() {
     const cannonDebugger = new CannonDebugger(scene, world, {
       color: "#AEE2FF",
       scale: 1
-    })
+    });
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 4.5;
     camera.position.y = 1.5;
 
-
     const randomRangeNum = (max, min) => {
-      return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    const isPositionOccupied = (position, objects) => {
+      return objects.some(obj => {
+        const distance = position.clone().sub(obj.body.position).length();
+        return distance < 2; // Adjust this threshold based on object sizes
+      });
+    };
 
     const moveObstacles = (arr, speed, maxX, minX, maxZ, minZ) => {
       arr.forEach((e) => {
         e.body.position.z += speed;
         if (e.body.position.z > camera.position.z) {
-          e.body.position.x = randomRangeNum(maxX, minX)
-          e.body.position.z = randomRangeNum(maxZ, minZ)
+          e.body.position.x = randomRangeNum(maxX, minX);
+          e.body.position.z = randomRangeNum(maxZ, minZ);
         }
-        e.mesh.position.copy(e.body.position)
-        e.mesh.quaternion.copy(e.body.quaternion)
+        e.mesh.position.copy(e.body.position);
+        e.mesh.quaternion.copy(e.body.quaternion);
       });
-    }
-
+    };
 
     // Ground
-
     const groundBody = new CANNON.Body({
-      shape: new CANNON.Box(new CANNON.Vec3(15, 0.5, 15)),
-    })
-    groundBody.position.y = 0
+      shape: new CANNON.Box(new CANNON.Vec3(15, 0.5, 150)),
+    });
+    groundBody.position.y = 0;
     world.addBody(groundBody);
 
     const ground = new THREE.Mesh(
-      new THREE.BoxGeometry(30, 1, 30),
+      new THREE.BoxGeometry(30, 1, 300),
       new THREE.MeshBasicMaterial({ color: 0x91785d })
-    )
-    ground.position.y = 0
+    );
+    ground.position.y = 0;
     scene.add(ground);
-
-
 
     // Player
     const playerBody = new CANNON.Body({
       mass: 1,
       shape: new CANNON.Box(new CANNON.Vec3(0.25, 0.25, 0.25)),
       fixedRotation: true
-    })
-    playerBody.position.y = 1
-    world.addBody(playerBody)
+    });
+    playerBody.position.y = 1;
+    world.addBody(playerBody);
 
     const player = new THREE.Mesh(
       new THREE.BoxGeometry(0.5, 0.5, 0.5),
       new THREE.MeshBasicMaterial({ color: 0xf00000 })
-    )
+    );
     scene.add(player);
-
-    playerBody.addEventListener("collide", (e) => {
-      powerups.forEach((el) => {
-        if (e.body === el.body) {
-          el.body.position.x = randomRangeNum(8, -8);
-          el.body.position.z = randomRangeNum(-5, -10);
-          el.mesh.position.copy(el.body.position);
-          el.mesh.quaternion.copy(el.body.quaternion);
-          points += 1;
-          pointsUI.textContent = points.toString();
-        }
-      })
-      enemies.forEach((el) => {
-        if (e.body === el.body) {
-          gameOver = true
-        }
-      })
-    })
-
-    //Enemies
-    // Ennemis
-    const enemies = [];
-    for (let i = 0; i < 2; i++) {
-      const posX = randomRangeNum(8, -8);
-      const posZ = randomRangeNum(-5, -10);
-
-      const enemy = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0x0000ff })
-      );
-      enemy.position.y = 1
-      enemy.position.x = posX;
-      enemy.position.z = posZ;
-      scene.add(enemy);
-
-      const enemyBody = new CANNON.Body({
-        mass: 1,
-        shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
-      });
-      enemyBody.position.set(posX, 1, posZ); // Assurez-vous de donner une hauteur
-      world.addBody(enemyBody);
-
-      const enemyObject = {
-        mesh: enemy,
-        body: enemyBody,
-      };
-
-      enemies.push(enemyObject);
-    }
 
     // Powerups
     const powerups = [];
-    for (let i = 0; i < 5; i++) {
-      const posX = randomRangeNum(8, -8);
-      const posZ = randomRangeNum(-5, -10);
+    const enemies = [];
+
+    for (let i = 0; i < 10; i++) {
+      let posX, posZ, position;
+
+      do {
+        posX = randomRangeNum(1, -1);
+        posZ = randomRangeNum(-10, -40);
+        position = new THREE.Vector3(posX, 1, posZ);
+      } while (isPositionOccupied(position, powerups.concat(enemies)));  // Check against powerups and enemies
 
       const powerup = new THREE.Mesh(
         new THREE.TorusGeometry(1, 0.4, 16, 50),
         new THREE.MeshBasicMaterial({ color: 0xffff00 })
       );
       powerup.scale.set(0.1, 0.1, 0.1);
-      powerup.position.set(posX, 1, posZ); // Fixez la position à une hauteur raisonnable
+      powerup.position.set(posX, 1, posZ);
       scene.add(powerup);
 
       const powerupBody = new CANNON.Body({
@@ -159,46 +119,92 @@ function App() {
       powerups.push(powerupObject);
     }
 
+    // Enemies
 
+    for (let i = 0; i < 2; i++) {
+      let posX, posZ, position;
 
+      do {
+        posX = randomRangeNum(1, -1);
+        posZ = randomRangeNum(-5, -10);
+        position = new THREE.Vector3(posX, 1, posZ);
+      } while (isPositionOccupied(position, powerups.concat(enemies)));  // Check against powerups and enemies
 
+      const enemy = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({ color: 0x0000ff })
+      );
+      enemy.position.y = 1;
+      enemy.position.set(posX, 1, posZ);
+      scene.add(enemy);
 
+      const enemyBody = new CANNON.Body({
+        mass: 1,
+        shape: new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5)),
+      });
+      enemyBody.position.set(posX, 1, posZ);
+      world.addBody(enemyBody);
+
+      const enemyObject = {
+        mesh: enemy,
+        body: enemyBody,
+      };
+
+      enemies.push(enemyObject);
+    }
+
+    // Listen for collisions after enemies and powerups have been initialized
+    playerBody.addEventListener("collide", (e) => {
+      powerups.forEach((el) => {
+        if (e.body === el.body) {
+          el.body.position.x = randomRangeNum(8, -8);
+          el.body.position.z = randomRangeNum(-10, -40);
+          el.mesh.position.copy(el.body.position);
+          el.mesh.quaternion.copy(el.body.quaternion);
+          points += 1;
+          pointsUI.textContent = points.toString();
+        }
+      });
+      enemies.forEach((el) => {
+        if (e.body === el.body) {
+          gameOver = true;
+        }
+      });
+    });
 
     // Animation loop
     function animate() {
-
       requestAnimationFrame(animate);
 
       if (!gameOver) {
-        moveObstacles(powerups, 0.1, 8, -8, -5, -10)
-        moveObstacles(enemies, 0.2, 8, -8, -5, -10)
-      }
-      else {
+        moveObstacles(powerups, 0.3, 1, -1, -10, -40);
+        moveObstacles(enemies, 0.3, 1, -1, -10, -40);
+      } else {
         pointsUI.textContent = "GAME OVER";
         playerBody.velocity.set(playerBody.position.x, 5, 5);
 
         enemies.forEach((el) => {
           scene.remove(el.mesh);
           world.removeBody(el.body);
-        })
+        });
 
         powerups.forEach((el) => {
           scene.remove(el.mesh);
           world.removeBody(el.body);
-        })
+        });
 
         if (playerBody.position.z > camera.position.z) {
-          scene.remove(player)
-          world.removeBody(playerBody)
+          scene.remove(player);
+          world.removeBody(playerBody);
         }
       }
 
       world.fixedStep();
 
-      player.position.copy(playerBody.position)
-      player.quaternion.copy(playerBody.quaternion)
+      player.position.copy(playerBody.position);
+      player.quaternion.copy(playerBody.quaternion);
 
-      cannonDebugger.update()
+      cannonDebugger.update();
 
       renderer.render(scene, camera);
     }
@@ -210,8 +216,8 @@ function App() {
     window.addEventListener("resize", () => {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight)
-    })
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    });
 
     window.addEventListener("keydown", (e) => {
       if (e.key === "d" || e.key === "D" || e.key === "ArrowRight") {
@@ -221,12 +227,12 @@ function App() {
         playerBody.position.x -= 1;
       }
       if (e.key === "r" || e.key === "R") {
-        playerBody.position.z = 0
-        playerBody.position.x = 0
-        playerBody.position.y = 0
+        playerBody.position.z = 0;
+        playerBody.position.x = 0;
+        playerBody.position.y = 0;
       }
       if (e.key === " ") {
-        playerBody.position.y += 2
+        playerBody.position.y += 2;
       }
       if (playerBody.position.x > 1 && (e.key === "d" || e.key === "D" || e.key === "ArrowRight")) {
         playerBody.position.x = 1;
@@ -234,7 +240,7 @@ function App() {
       if (playerBody.position.x < -1 && (e.key === "q" || e.key === "Q" || e.key === "ArrowLeft")) {
         playerBody.position.x = -1;
       }
-    })
+    });
 
     // Cleanup function to remove the renderer on component unmount
     return () => {
