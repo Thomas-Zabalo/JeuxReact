dee9de8bc0b3766c9301b69ec32523ae28988194
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-12-17 09:03:45 +0100

    file main

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer class="bg-white rounded-lg shadow">
+            <div class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
+                <span class="text-sm text-gray-500 sm:text-center ">© 2024 <a href="https://mmi.iut-tlse3.fr/" class="hover:underline">MMI CASTRES™</a>. Tous droit réservé.</span>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
