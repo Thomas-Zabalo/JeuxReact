323597da9496fbe646bbad8e23a5c2305e8f9e0a
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-12-23 15:21:39 +0100

    Rework after merge

--- src/projects/sonic/entities/sonicexe.js
+++ src/projects/sonic/entities/sonicexe.js
@@ -1,15 +1,15 @@
 import k from "../kaplayCtx";
 
-export function makefish(pos){
+export function makeSonicexe(pos){
     return k.add([
-        k.sprite("fish", {anim: "fishing"}),
+        k.sprite("sonicexe", {anim: "idles"}),
         k.area({shape: new k.Rect(k.vec2(-5, 0), 32, 32)}),
         k.scale(4),
         k.anchor("center"),
         k.pos(pos),
         k.offscreen(),
         k.animate(),
-        "enemyfish",
-        {z: 10}
+        "exe",
+        {z: 11}
     ]);
 }
